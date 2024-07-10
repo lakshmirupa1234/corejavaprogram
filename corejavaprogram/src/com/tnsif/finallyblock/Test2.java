6b5de4755dae7393f7f4323779d98181f87bd420
admin <admin@DESKTOP-V09RESP.Dlink>
2024-07-09 23:32:07 -0700

    commit message

---
+++ corejavaprogram/src/com/tnsif/finallyblock/Test2.java
@@ -0,0 +1,17 @@
+package com.tnsif.finallyblock;
+//exception with no matching catch block
+public class Test2 {
+    public static void main(String[] args) {
+		try {
+			System.out.println(100/0);
+		}
+		catch(NullPointerException npe) {
+			System.out.println("exception caught");
+		}
+		finally {
+			System.out.println("welcome");
+		}
+
+	}
+
+}
